e8079214dacdf08fbf4821c625e0819fcb737414
Kester Dobson <kdobson@gmail.com>
2009-06-24 17:54:20 +0100

    Fixed pagination

--- controller/main.rb
+++ controller/main.rb
@@ -13,12 +13,17 @@ class MainController < Controller
 
 
     if arg1 == "page" or arg1 == nil
+      @title = CONFIG.blog.name
+      
       @offset = arg2.to_i
+      @page_count = (Post.select(:ID).filter(:post_status => 'publish').filter(:post_type => 'post').count / CONFIG.blog.posts_per_page.to_i) + 1
     
-      @posts = Post.filter(:post_status => 'publish').filter(:post_type => 'post').order(:post_date.desc).paginate(@offset, 10)
+      @posts = Post.filter(:post_status => 'publish').filter(:post_type => 'post').order(:post_date.desc).paginate(@offset, CONFIG.blog.posts_per_page.to_i)
       
     elsif arg1 == "tag" and arg2 != nil
      
+      @title = CONFIG.blog.name + " - Posts tagged with \"#{arg2}\""
+      
       @posts = Post.fetch("SELECT * FROM wp_posts INNER JOIN wp_term_relationships on wp_posts.ID = wp_term_relationships.object_id INNER JOIN wp_term_taxonomy on wp_term_relationships.term_taxonomy_id = wp_term_taxonomy.term_taxonomy_id INNER JOIN wp_terms ON wp_term_taxonomy.term_id = wp_terms.term_id WHERE wp_terms.slug = '#{arg2}' AND wp_term_taxonomy.taxonomy = 'post_tag' AND wp_posts.post_status = 'publish' and wp_posts.post_type = 'post' ORDER BY wp_posts.post_modified DESC").all
    
     elsif arg1 != nil and @post = Post[:post_name => arg1]
